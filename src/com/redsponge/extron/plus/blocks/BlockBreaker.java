3028e0ee65761eae5b129ff10ffb3a8de29113bf
BuildTools <unconfigured@null.spigotmc.org>
2018-03-25 19:00:20 +0300

    Hello!

---
+++ src/com/redsponge/extron/plus/blocks/BlockBreaker.java
@@ -0,0 +1,35 @@
+package com.redsponge.extron.plus.blocks;
+
+import org.bukkit.Material;
+
+public class BlockBreaker extends CustomBlockContainer {
+
+    @Override
+    public String getId() {
+        return "block_breaker";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Block Breaker";
+    }
+
+    @Override
+    public Material getRealBlockType() {
+        return Material.DROPPER;
+    }
+
+    @Override
+    public byte getRealBlockData() {
+        return 0;
+    }
+
+    @Override
+    public void onRedstoneActivated() {
+        this.activate();
+    }
+
+    private void activate() {
+
+    }
+}
---
+++ src/com/redsponge/extron/plus/utils/Reference.java
@@ -0,0 +1,7 @@
+package com.redsponge.extron.plus.utils;
+
+public class Reference {
+
+    public static final String TRAPPED_SIGN_NAME = "trapped_sign";
+
+}
