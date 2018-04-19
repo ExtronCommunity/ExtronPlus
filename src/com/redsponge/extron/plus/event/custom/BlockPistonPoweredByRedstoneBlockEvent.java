9784964de5e84982d7fafd0ad78de6b28a2c51bf
unknown <redspongeyt@gmail.com>
2018-04-20 00:11:23 +0300

    Finishing movable mob spawners

--- src/com/redsponge/extron/plus/event/custom/BlockPistonPoweredByRedstoneBlockEvent.java
+++ src/com/redsponge/extron/plus/event/custom/BlockPistonPoweredByRedstoneBlockEvent.java
@@ -1,19 +1,46 @@
 package com.redsponge.extron.plus.event.custom;
 
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockPistonEvent;
 
-public class BlockPistonPoweredEvent extends BlockEvent {
+public class BlockPistonPoweredByRedstoneBlockEvent extends BlockEvent {
 
-    private static final HandlerList handles = new HandlerList();
-
-    public BlockPistonPoweredEvent(Block theBlock) {
-        super(theBlock);
+    private static final HandlerList handlers = new HandlerList();
+    private boolean sticky;
+    private Block poweredBy;
+    private BlockFace direction;
+    public BlockPistonPoweredByRedstoneBlockEvent(Block piston, BlockFace direction, Block poweredBy, boolean sticky) {
+        super(piston);
+        this.direction = direction;
+        this.poweredBy = poweredBy;
+        this.sticky = sticky;
     }
 
     @Override
     public HandlerList getHandlers() {
-        return null;
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public boolean isSticky() {
+        return sticky;
+    }
+
+    public Block getPiston() {
+        return block;
+    }
+
+    public BlockFace getDirection() {
+        return direction;
+    }
+
+    public Block getPoweredBy() {
+        return poweredBy;
     }
 }
