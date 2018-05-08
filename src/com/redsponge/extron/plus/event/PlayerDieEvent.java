d305de5b453d8cb8e646bf0867c5ce67cdb3232c
RedSponge <ThatRedSponge@gmail.com>
2018-05-08 21:11:53 +0300

    Die Event

--- src/com/redsponge/extron/plus/event/PlayerDieEvent.java
+++ src/com/redsponge/extron/plus/event/PlayerDieEvent.java
@@ -4,13 +4,14 @@ import com.redsponge.extron.plus.ExtronPlus;
 import com.redsponge.extron.plus.utils.Reference;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerDieEvent implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e) {
-        if(ExtronPlus.INSTANCE.getJetpackHandler().wearingJetpack(e.getEntity())) {
+        if(ExtronPlus.INSTANCE.getJetpackHandler().wearingJetpack(e.getEntity()) && e.getEntity().getLastDamageCause().getCause() == DamageCause.FALL || e.getEntity().getLastDamageCause().getCause() == DamageCause.FLY_INTO_WALL) {
             e.setDeathMessage(Reference.DEATH_MESSAGE_JETPACK.replaceAll("%name", e.getEntity().getDisplayName()));
         }
         ExtronPlus.INSTANCE.getJetpackHandler().playerDeath(e.getEntity());
