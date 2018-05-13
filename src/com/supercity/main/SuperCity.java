feaee6eea42134fe1244392ae8f970746b1b0d67
TheShinyBunny <shinybunny123@gmail.com>
2018-05-13 22:26:51 +0200

    Added creeper checker

--- src/com/supercity/main/SuperCity.java
+++ src/com/supercity/main/SuperCity.java
@@ -9,6 +9,9 @@ import com.supercity.main.config.ConfigManager;
 import com.supercity.main.crafting.RecipeBackpack;
 import com.supercity.main.crafting.RecipeCraftingStick;
 import com.supercity.main.crafting.RecipeJetpack;
+import com.supercity.main.creepers.CommandCreeperCheck;
+import com.supercity.main.creepers.CreeperChecker;
+import com.supercity.main.creepers.CreeperEvents;
 import com.supercity.main.enchants.Boing;
 import com.supercity.main.enchants.CurseOfBreaking;
 import com.supercity.main.enchants.HeatWalker;
@@ -55,16 +58,18 @@ public class SuperCity extends JavaPlugin implements Listener {
         registerHandlers();
 
         initiatePlayers();
-        getLogger().info(ChatColor.GREEN.toString() + "Extron Plus has been successfully loaded!");
+        getLogger().info(ChatColor.GREEN.toString() + "Super City has been successfully loaded!");
         registerEnchants();
         ConfigManager.init();
         ItemBackPack.loadAllBackpacks();
 
         registerRecipes();
+        CreeperChecker.load();
     }
 
     public void onDisable() {
         unregisterEnchants();
+        CreeperChecker.save();
         //RecordingManager.setAllNotAFK();
     }
 
@@ -86,12 +91,14 @@ public class SuperCity extends JavaPlugin implements Listener {
         //Bukkit.getScheduler().scheduleSyncRepeatingTask(SuperCity.INSTANCE, RecordingManager::tick, 0, 1);
         pm.registerEvents(new PlayerCloseInventoryEvent(), this);
         pm.registerEvents(new PlayerCraftItemEvent(), this);
+        pm.registerEvents(new CreeperEvents(),this);
     }
 
     private void registerCommands() {
         getCommand("getCustomItem").setExecutor(new CommandGetCustomItem());
         getCommand("dontSkipNight").setExecutor(new CommandDontSkipNight());
         getCommand("enableOnePlayerSleep").setExecutor(new CommandReEnableOnePlayerSleep());
+        getCommand("creeper").setExecutor(new CommandCreeperCheck());
         //getCommand("recording").setExecutor(new CommandRecording());
         //getCommand("togglescoreboard").setExecutor(new CommandToggleSB());
         //getCommand("togglechat").setExecutor(new CommandToggleChat());
--- src/com/supercity/main/utils/WorldUtils.java
+++ src/com/supercity/main/utils/WorldUtils.java
@@ -1,6 +1,7 @@
 package com.supercity.main.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 
 public class WorldUtils {
@@ -9,4 +10,44 @@ public class WorldUtils {
         return Bukkit.getWorlds().get(0);
     }
 
+    public static Location parseLocation(String locString) {
+        String[] strings = locString.split(",");
+        if (strings.length < 4) {
+            return null;
+        }
+        World w = Bukkit.getWorld(strings[0]);
+        if (w == null) {
+            return null;
+        }
+        try {
+            int x = Integer.parseInt(strings[1]);
+            int y = Integer.parseInt(strings[2]);
+            int z = Integer.parseInt(strings[3]);
+            return new Location(w,x,y,z);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static Location parseLocation(String locString, World w) {
+        String[] strings = locString.split(",");
+        if (strings.length < 3) {
+            return null;
+        }
+        try {
+            int x = Integer.parseInt(strings[0]);
+            int y = Integer.parseInt(strings[1]);
+            int z = Integer.parseInt(strings[2]);
+            return new Location(w,x,y,z);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static String encodeLocation(Location loc, boolean world) {
+        if (loc == null) {
+            return null;
+        }
+        return (world ? loc.getWorld().getName() + "," : "") + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+    }
 }
