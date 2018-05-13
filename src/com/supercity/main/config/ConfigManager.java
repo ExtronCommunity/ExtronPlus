ce0e81aedfffd968d3bcfc1b62e36b138c5c257b
TheShinyBunny <shinybunny123@gmail.com>
2018-05-13 19:40:13 +0200

    WIP trapped signs

--- src/com/supercity/main/config/ConfigManager.java
+++ src/com/supercity/main/config/ConfigManager.java
@@ -1,16 +1,31 @@
 package com.supercity.main.config;
 
 import com.supercity.main.SuperCity;
+import com.supercity.main.utils.Reference;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigManager {
 
     public static FileConfiguration config;
     public static ConfigFile backpackConfig;
+    public static ConfigFile trappedSignsConfig;
 
     public static void init() {
         config = SuperCity.INSTANCE.getConfig();
         backpackConfig = new ConfigFile("backpacks.yml");
+        trappedSignsConfig = new ConfigFile("trapped_signs.yml");
+        List<Location> locations = Reference.TRAPPED_SIGNS_LOCATIONS;
+        for (String s : trappedSignsConfig.get().getKeys(false)) {
+            ConfigurationSection section = trappedSignsConfig.get().getConfigurationSection(s);
+            Location loc = new Location(Bukkit.getWorld(section.getString("world")),section.getInt("x"),section.getInt("y"),section.getInt("z"));
+            locations.add(loc);
+        }
     }
 
     public static void saveConfig() {
--- src/com/supercity/main/event/PlayerBreakBlock.java
+++ src/com/supercity/main/event/PlayerBreakBlock.java
@@ -1,5 +1,6 @@
 package com.supercity.main.event;
 
+import com.supercity.main.utils.Reference;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,8 +11,8 @@ public class PlayerBreakBlock implements Listener {
 
     @EventHandler
     public void breakBlock(BlockBreakEvent e) {
-        if (PlayerInteractionEvent.trappedSigns.contains(e.getBlock().getLocation())) {
-            PlayerInteractionEvent.trappedSigns.remove(e.getBlock().getLocation());
+        if (Reference.TRAPPED_SIGNS_LOCATIONS.contains(e.getBlock().getLocation())) {
+            Reference.TRAPPED_SIGNS_LOCATIONS.remove(e.getBlock().getLocation());
             e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(),new ItemStack(Material.TRIPWIRE_HOOK));
         }
     }
